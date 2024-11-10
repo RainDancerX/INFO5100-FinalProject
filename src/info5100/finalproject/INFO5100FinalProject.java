e008f34c60124b82044b7cc8989587a21166a237
lucas Liu <lantasy.io@gmail.com>
2024-11-10 02:19:23 -0800

    initial commit

---
+++ src/info5100/finalproject/INFO5100FinalProject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package info5100.finalproject;
+
+/**
+ *
+ * @author lucas
+ */
+public class INFO5100FinalProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
